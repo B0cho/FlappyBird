b158cde0420e3b4d5c8f0b1cc7bb813d077b1b16
B0cho <obiekt44@gmail.com>
2021-12-25 18:32:58 +0100

    Bird flying physics implemented

---
+++ Assets/birdBehavior.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class birdBehavior : MonoBehaviour
+{
+    public float birdWeight;
+    public float jumpSpeed;
+
+    private float verticalSpeed;
+    private float currentAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        verticalSpeed = 0;
+        currentAngle = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalSpeed -= birdWeight * 9.8f; 
+        if (Input.GetKeyDown("space"))
+            verticalSpeed = jumpSpeed;
+
+        float newAngle  = Mathf.Atan(verticalSpeed) * 100;
+        float rotation = newAngle - currentAngle;
+        currentAngle = newAngle;             
+        
+        gameObject.transform.position = gameObject.transform.position + new Vector3(0, verticalSpeed, 0);
+        gameObject.transform.RotateAround(gameObject.transform.position, Vector3.forward, rotation);
+    }
+}
